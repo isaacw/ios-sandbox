80ee125b0f3c5c22ae817f2297733fe94e533f74
Isaac Weinhausen <me@isaacw.com>
2014-10-01 22:02:37 -0700

    Added Scratchpad playground with Swift recap code

---
+++ FunFacts/Scratchpad.playground/section-1.swift
@@ -0,0 +1,24 @@
+// Scratchpad
+
+import UIKit
+
+
+
+// Swift Basics Recap
+
+var someVar = "a variable"
+let someConst = 20
+
+someVar = "another string"
+
+var fruitsArray = ["Apples"]
+fruitsArray.append("Bananas")
+
+fruitsArray
+
+
+
+// Optionals
+
+var optionalString: String? = "hello"
+optionalString = nil
