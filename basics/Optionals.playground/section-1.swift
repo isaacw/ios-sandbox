1f3a60005176466e5dcf7acf1361b86bc762ec04
Isaac Weinhausen <me@isaacw.com>
2014-10-01 20:53:43 -0700

    Example of Optional Chaining

--- basics/Optionals.playground/section-1.swift
+++ basics/Optionals.playground/section-1.swift
@@ -2,6 +2,10 @@
 
 import UIKit
 
+func sendNoticeTo (#apt: Int) {
+    
+}
+
 func findApt (aptNum: String) -> String? {
     let aptNumbers = ["101","202","303","404"]
     for n in aptNumbers {
@@ -13,8 +17,7 @@ func findApt (aptNum: String) -> String? {
     return nil
 }
 
-if let culprit = findApt("101") {
-    println("apt found! \(culprit)")
+if let culprit = findApt("101")?.toInt() {
+    sendNoticeTo(apt: culprit)
 }
-
 
